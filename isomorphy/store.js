cdb505506712357cdfb86c0357314f40c2e14bce
Gerald <me@jacobford.co.uk>
2020-11-25 12:36:21 +0000

    feat: update store dispatch logic

--- isomorphy/store.js
+++ isomorphy/store.js
@@ -1,16 +1,29 @@
 export const createStore = () => {
-	let state = null;
+  let state = null;
 
-	const dispatch = (fn) => state = fn(state);
+  const dispatch = (fn) => (state = fn(state));
 
-	const getState = () => state;
+  const getState = () => state;
 
-	const save = () => {}
+  const save = () => {};
 
-	return { save, getState, dispatch }
-}
+  return { save, getState, dispatch };
+};
 
-export const store = createStore();
+const store = createStore();
 
-export const addRoute = (newRoute) => store.dispatch(state => ({ ...state, routes: [...state.routes, newRoute] }));
-export const addEntity = (entity) => store.dispatch(state => ({ ...state, [entity.name]: entity }));
+export const addRoute = (newRoute) => {
+  store.dispatch((state) => ({
+    ...state,
+    routes: [...state.routes, newRoute],
+  }));
+};
+
+export const addEntity = (entity) => {
+  const currentEntities = state.entities;
+  const entities = { ...currentEntities, entity };
+  store.dispatch((state) => ({ ...state, entities }));
+};
+
+export const getState = store.getState;
+
